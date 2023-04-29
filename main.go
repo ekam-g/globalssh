f5b4c82413baf0f3a714a311894c95208fef7fe4
carghai <carghai74@gmail.com>
2023-04-29 14:31:07 -0400

    testing out go

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("boilerplate")
+	cmd := exec.Command("echo", "hello")
+	cmdReader, _ := cmd.StdoutPipe()
+	scanner := bufio.NewScanner(cmdReader)
+	done := make(chan bool)
+	go func() {
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+		done <- true
+	}()
+	err := cmd.Start()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	<-done
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
