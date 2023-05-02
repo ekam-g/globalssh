8ee5be2b93b03d04e8fdf586a934f7a5506caaa5
carghai <carghai74@gmail.com>
2023-05-01 22:52:16 -0400

    base code works need changeds

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module Global_SSH_V2
 
 go 1.20
+
+require (
+	github.com/creack/pty v1.1.18 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/term v0.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,30 +1,59 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
+	"io"
 	"log"
+	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
+
+	"github.com/creack/pty"
+	"golang.org/x/term"
 )
 
-func main() {
-	cmd := exec.Command("bash", "-c", "neofetch")
+func test() error {
+	// Create arbitrary command.
+	c := exec.Command("bash")
 
-	outPipe, _ := cmd.StdoutPipe()
-	stdErrorPipe, _ := cmd.StderrPipe()
-	scanner := bufio.NewScanner(outPipe)
-	stdErrorScanner := bufio.NewScanner(stdErrorPipe)
+	// Start the command with a pty.
+	ptmx, err := pty.Start(c)
+	if err != nil {
+		return err
+	}
+	// Make sure to close the pty at the end.
+	defer func() { _ = ptmx.Close() }() // Best effort.
+
+	// Handle pty size.
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGWINCH)
 	go func() {
-		for scanner.Scan() {
-			if scanner.Text() != "" {
-				fmt.Println(scanner.Text())
-			} else {
-				log.Println(stdErrorScanner.Text())
-
+		for range ch {
+			if err := pty.InheritSize(os.Stdin, ptmx); err != nil {
+				log.Printf("error resizing pty: %s", err)
 			}
 		}
 	}()
-	cmd.Start()
-	cmd.Wait()
-	log.Println("Done")
+	ch <- syscall.SIGWINCH                        // Initial resize.
+	defer func() { signal.Stop(ch); close(ch) }() // Cleanup signals when done.
+
+	// Set stdin in raw mode.
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	defer func() { _ = term.Restore(int(os.Stdin.Fd()), oldState) }() // Best effort.
+
+	// Copy stdin to the pty and the pty to stdout.
+	// NOTE: The goroutine will keep reading until the next keystroke before returning.
+	go func() { _, _ = io.Copy(ptmx, os.Stdin) }()
+	_, _ = io.Copy(os.Stdout, ptmx)
+
+	return nil
+}
+
+func main() {
+	if err := test(); err != nil {
+		log.Fatal(err)
+	}
 }
