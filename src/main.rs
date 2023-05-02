f0ac3897506d4df632ed4aa18e5338fa6a02fae2
carghai <carghai74@gmail.com>
2023-05-02 15:40:31 -0400

    works with rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use ptyprocess::PtyProcess;
+use std::io::{BufRead, BufReader, Result, Write};
+use std::process::Command;
+use std::thread::{self};
+use std::time::Duration;
+
+fn main() -> Result<()> {
+    // spawn a cat process
+    let mut process = PtyProcess::spawn(Command::new("zsh"))?;
+
+    // create a communication stream
+    let mut stream = process.get_raw_handle()?;
+
+    // send a message to process
+    writeln!(stream, "neofetch")?;
+
+    // read a line from the stream
+    let mut reader = BufReader::new(stream);
+    let mut buf = String::new();
+    loop {
+        reader.read_line(&mut buf)?;
+        println!("{buf}");
+    }
+
+    // stop the process
+    assert!(process.exit(true)?);
+
+    Ok(())
 }
