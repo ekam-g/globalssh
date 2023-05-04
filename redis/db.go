dd6e3bba14debff5eba431a62b8a1d0d46ed5285
carghai <carghai74@gmail.com>
2023-05-04 14:20:33 -0400

    stuff

--- redis/db.go
+++ redis/db.go
@@ -1,5 +1,15 @@
 package redis
 
+import (
+	"github.com/go-redis/redis"
+)
+
 func init() {
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
 
 }
