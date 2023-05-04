043059a481beb868d454bdec00f004ec4af4e389
carghai <carghai74@gmail.com>
2023-05-04 15:58:50 -0400

    all errors cleared

--- redis/db.go
+++ redis/db.go
@@ -4,13 +4,14 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+var client *redis.Client
+
 func init() {
 	key := GetKey()
-	client := redis.NewClient(&redis.Options{
+	client = redis.NewClient(&redis.Options{
 		Addr:     key.Addr,
 		Username: key.Username,
 		Password: key.Password,
 		DB:       key.DB,
 	})
-
 }
