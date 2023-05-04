036f5c948ee6cb92a335dcaac2241733c512afd6
carghai <carghai74@gmail.com>
2023-05-04 14:01:33 -0400

    adding redis code

--- redis/setup.go
+++ redis/setup.go
@@ -1,5 +1,31 @@
 package redis
 
-func start() {
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
+const (
+	db_key_location = "redis_key"
+)
+
+func get_key() string {
+	redis_key_file, err := os.ReadFile(db_key_location)
+	if err != nil {
+		log.Print("Failed to Find Old Redis Key, Please enter new one: ")
+		file, err := os.Create(db_key_location)
+		if err != nil {
+			log.Fatal("Failed To Create File Due to: ", err)
+		}
+		var key string
+		fmt.Scan(&key)
+		file.Write([]byte(key))
+		err = file.Close()
+		if err != nil {
+			log.Fatal("Failed to write data due to:", err)
+		}
+		return key
+	}
+	return string(redis_key_file)
 }
