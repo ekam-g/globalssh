4acc2c73a34e252373851e436916f619e5d857aa
carghai <carghai74@gmail.com>
2023-05-04 12:46:44 -0400

    testing

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"os"
 	"os/exec"
 	"time"
 
@@ -16,19 +17,31 @@ func main() {
 		panic(err)
 	}
 	f.Write([]byte("neofetch\n"))
-	go func() {
-		for {
-			buf := make([]byte, 1024)
-			n, err := f.Read(buf)
-			if err != nil {
-				if err != io.EOF {
-					panic(err)
-				}
-				break
-			}
-			fmt.Print(string(buf[:n]))
-		}
-	}()
+	go reader(f)
+	command(f)
 	// f.Write([]byte("ls\n"))
 	time.Sleep(time.Second * 5)
+}
+
+func command(f *os.File) {
+	for {
+		var input string
+		fmt.Scan(&input)
+		input += "\n"
+		f.Write([]byte(input))
+	}
+}
+
+func reader(f *os.File) {
+	for {
+		buf := make([]byte, 1024)
+		n, err := f.Read(buf)
+		if err != nil {
+			if err != io.EOF {
+				panic(err)
+			}
+			break
+		}
+		fmt.Print(string(buf[:n]))
+	}
 }
