11ce9634c58fbd1e44db463c5cec8fffd73be374
carghai <carghai74@gmail.com>
2023-05-05 17:36:15 -0400

    cleint is made

---
+++ client/master.go
@@ -0,0 +1,35 @@
+package client
+
+import (
+	"fmt"
+	"global_ssh_v2/redis"
+	"log"
+)
+
+func Run() {
+	go display()
+	input()
+}
+
+func display() {
+	for {
+		data := redis.AwaitData(false)
+		fmt.Print(data)
+	}
+
+}
+
+func input() {
+	for {
+		var input string
+		_, err := fmt.Scan(&input)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		err = redis.Send(input, true)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
