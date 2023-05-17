1e38362307913c0bff1d1761f15f7eb8be758110
carghai <carghai74@gmail.com>
2023-05-16 21:33:29 -0400

    new thing

--- db/workers.go
+++ db/workers.go
@@ -9,21 +9,39 @@ import (
 )
 
 func SenderWorker(data chan string, HostMode bool, client *redis.Client) {
-	var allowSend sync.Mutex
-	redis_send := make(chan string)
-	go send(redis_send, client, &allowSend)
+	var waiting_data_mx sync.Mutex
+	run := make(chan struct{})
 	waiting_data := ""
+	go waitingWorker(run, &waiting_data, &waiting_data_mx, HostMode, client)
 	for {
 		send_data := <-data
+		waiting_data_mx.Lock()
 		waiting_data += send_data
-		if !mutexLocked(&allowSend) {
-			redis_send <- waiting_data
-			waiting_data = ""
+		waiting_data_mx.Unlock()
+		run <- struct{}{}
+	}
+
+}
+
+func waitingWorker(wait chan struct{}, waiting_data *string, waiting_data_mx *sync.Mutex, HostMode bool, client *redis.Client) {
+	var allowSend sync.Mutex
+	redis_send := make(chan string)
+	go send(redis_send, client, &allowSend, HostMode)
+	for {
+		<-wait
+		waiting_data_mx.Lock()
+		if *waiting_data == "" {
+			continue
 		}
+		if !mutexLocked(&allowSend) {
+			redis_send <- *waiting_data
+			*waiting_data = ""
+		}
+		waiting_data_mx.Unlock()
 	}
 }
 
-func send(data chan string, client *redis.Client, allowSend *sync.Mutex) {
+func send(data chan string, client *redis.Client, allowSend *sync.Mutex, HostMode bool) {
 	for {
 		send_data := <-data
 		allowSend.Lock()
