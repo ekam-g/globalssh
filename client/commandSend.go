76e078fa3178edde1042b014b3ea802533550076
carghai <carghai74@gmail.com>
2023-08-18 17:01:44 -0400

    done just need docs

--- client/commandSend.go
+++ client/commandSend.go
@@ -10,12 +10,19 @@ import (
 )
 
 func CommandSend(hostName string, wait string, data string) {
-	timeRead, err := strconv.Atoi(wait)
 	data = strings.ReplaceAll(data, "\\n", "\n")
+	Net, _ := net.Init(false, hostName)
+	timeRead, err := strconv.Atoi(wait)
 	if err != nil {
+		// if wait == "n" {
+		// 	err := Net.Send(data, net.Command)
+		// 	checkErr(err)
+		// 	result, err := Net.Read(net.Result)
+		// 	checkErr(err)
+
+		// }
 		log.Fatalf("Failed to Parse int due to %s\nExiting Program\n", err)
 	}
-	Net, _ := net.Init(false, hostName)
 	if timeRead == 0 {
 		Net.Send(data, net.Command)
 		os.Exit(0)
@@ -25,4 +32,10 @@ func CommandSend(hostName string, wait string, data string) {
 	go Net.Send(data, net.Command)
 	time.Sleep(time.Second * time.Duration(timeRead))
 	os.Exit(0)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalf("Exiting due to %s", err)
+	}
 }
