4478fa23b0b0f0ebab85552d234c315f5ce30c83
carghai <carghai74@gmail.com>
2023-10-13 10:05:38 -0400

    finish

--- src/pty/mod.rs
+++ src/pty/mod.rs
@@ -1,7 +1,7 @@
 use std::sync::mpsc::{Receiver, Sender};
 
 use anyhow::Error;
-use portable_pty::{CommandBuilder, NativePtySystem, PtySize, PtySystem};
+use portable_pty::{CommandBuilder, NativePtySystem, PtySize, PtySystem, Child};
 
 use std::{
     io::{BufRead, BufReader},
@@ -9,8 +9,8 @@ use std::{
 };
 #[derive(Debug)]
 pub struct PtyTerm {
-    input: Sender<String>,
-    output: Receiver<Result<String,std::io::Error>>,
+    pub input: Sender<String>,
+    pub output: Receiver<Result<String,std::io::Error>>,
 }
 
 impl PtyTerm {
@@ -25,28 +25,39 @@ impl PtyTerm {
                 pixel_height: 0,
             })?;
 
-        // Spawn a shell into the pty
         let cmd = CommandBuilder::new("zsh");
-        let mut child = pair.slave.spawn_command(cmd)?;
+        pair.slave.spawn_command(cmd)?;
         // Release any handles owned by the slave: we don't need it now
         // that we've spawned the child.
         drop(pair.slave);
-        // Read the output in another thread.
-        // This is important because it is easy to encounter a situation
-        // where read/write buffers fill and block either your process
-        // or the spawned process.
         let (tx, output) = channel();
         let reader = pair.master.try_clone_reader().unwrap();
         std::thread::spawn(move || {
             // Consume the output from the child
             let reader = BufReader::new(reader);
             for line in reader.lines() {
-                tx.send(line).expect("Unexpected Thread Closure, Crital Error");
+                tx.send(line).expect("Unexpected Thread Closure, Crital Failure");
             }
         });
-        return Ok(PtyTerm{
-            input: todo!(),
-            output,
+        let mut writer = pair.master.take_writer().unwrap();
+        let (input, rx) = channel();
+        if cfg!(target_os = "macos") {
+            // macOS quirk: the child and reader must be started and
+            // allowed a brief grace period to run before we allow
+            // the writer to drop. Otherwise, the data we send to
+            // the kernel to trigger EOF is interleaved with the
+            // data read by the reader! WTF!?
+            std::thread::sleep(std::time::Duration::from_millis(20));
+        }
+        std::thread::spawn(move || loop {
+            let data = rx.recv().expect("Critial Thread Error");
+            if let  Err(e) = writeln!(writer, "{data}") {
+                log::error!("Failed to write due to: {e}");
+            }
         });
+        Ok(PtyTerm{
+            input,
+            output,
+        })
     }
 }
